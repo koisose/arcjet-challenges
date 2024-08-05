79fa194b7294563b6a9bbf77d8fd892b62cd2e97
koisose <plokotokbelajar@gmail.com>
2024-08-05 20:05:21 +0700

    🚀 feat(): Add dependencies for @arcjet/next, @clerk/nextjs, and update next to 14.2.5. Adding dependencies for @arcjet/next, @clerk/nextjs, and updating next to 14.2.5. This allows for new functionality and improvements in the application.
    <emoji> <type>(<scope>): <subject>
    <body>
    
    With allowed <type> values are feat, fix, perf, docs, style, refactor, test, and build.

--- nextjs/src/pages/arcjet-challenge-accepted/index.tsx
+++ nextjs/src/pages/arcjet-challenge-accepted/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from "next";
-import { useEffect, useState } from "react";
+import arcjet, { tokenBucket } from "@arcjet/next";
 import Layout from "@/components/Layout";
 import NotRateLimited from "@/components/NotRateLimited";
 import RateLimited from "@/components/RateLimited";
@@ -14,10 +14,25 @@ export default function ArcjetChallengeAccepted({ rateLimited }: { rateLimited: 
     </Layout>
   );
 }
-
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
+  characteristics: ["ip.src"], // track requests by a custom user ID
+  rules: [
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      refillRate: 5, // refill 5 tokens per interval
+      interval: 60, // refill every 10 seconds
+      capacity: 10, // bucket maximum capacity of 10 tokens
+    }),
+  ],
+});
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { req } = context;
-
+  console.log(req)
+  // const userId = "user123"; // Replace with your authenticated user ID
+  const decision = await aj.protect(req, {  requested: 1 });
+  console.log("Arcjet decision", decision);
   /**
    * Complete the challenge and use Arcjet to set the value of rateLimited to true
    * when the user has been rate limited.
@@ -26,7 +41,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   return {
     props: {
-      rateLimited: false,
+      // rateLimited: false,
+      rateLimited: decision.isDenied()?true:false,
     },
   };
 };
